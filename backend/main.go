3b7d27e4622186607b7ac267166646d84be2e6db
Yoru <m.f.fetvaci@hotmail.com>
2024-06-10 22:46:36 +0300

    add CORS

--- backend/main.go
+++ backend/main.go
@@ -2,16 +2,30 @@ package main
 
 import (
 	gg "backend/api"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 
+	config := cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173", "http://meow:5173", "https://manga.yorunoken.com"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}
+
+	router.Use(cors.New(config))
+
 	api := router.Group("/api")
 	{
 		api.GET("/greet/:name", gg.Greet)
+		api.GET("/proxy-image", gg.ProxyImage)
 
 		api.GET("/manga", gg.GetManga)
 		api.GET("/manga/:uuid", gg.GetManga)
