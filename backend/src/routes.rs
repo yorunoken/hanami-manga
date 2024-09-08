e4e252ba5a046358cbd3f4eefd5914d538025841
Yoru <m.f.fetvaci@hotmail.com>
2024-09-09 01:16:52 +0300

    use Direct import

--- backend/src/routes.rs
+++ backend/src/routes.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::api;
 use sqlx::SqlitePool;
 use warp::Filter;
@@ -7,37 +9,37 @@ pub fn routes(
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     let get_manga = warp::path!("api" / "manga" / String)
         .and(warp::get())
-        .and(warp::query::<std::collections::HashMap<String, String>>())
+        .and(warp::query::<HashMap<String, String>>())
         .and_then(api::get_manga);
 
     let get_manga_collection = warp::path!("api" / "manga")
         .and(warp::get())
-        .and(warp::query::<std::collections::HashMap<String, String>>())
+        .and(warp::query::<HashMap<String, String>>())
         .and_then(api::get_manga_collection);
 
     let get_manga_aggregate = warp::path!("api" / "manga" / String / "aggregate")
         .and(warp::get())
-        .and(warp::query::<std::collections::HashMap<String, String>>())
+        .and(warp::query::<HashMap<String, String>>())
         .and_then(api::get_manga_aggregate);
 
     let get_chapter = warp::path!("api" / "chapter" / String)
         .and(warp::get())
-        .and(warp::query::<std::collections::HashMap<String, String>>())
+        .and(warp::query::<HashMap<String, String>>())
         .and_then(api::get_chapter);
 
     let get_chapter_collection = warp::path!("api" / "chapter")
         .and(warp::get())
-        .and(warp::query::<std::collections::HashMap<String, String>>())
+        .and(warp::query::<HashMap<String, String>>())
         .and_then(api::get_chapter_collection);
 
     let at_home_server = warp::path!("api" / "homeserver" / String)
         .and(warp::get())
-        .and(warp::query::<std::collections::HashMap<String, String>>())
+        .and(warp::query::<HashMap<String, String>>())
         .and_then(api::at_home_server);
 
     let proxy_image = warp::path!("api" / "proxyimage")
         .and(warp::get())
-        .and(warp::query::<std::collections::HashMap<String, String>>())
+        .and(warp::query::<HashMap<String, String>>())
         .and_then(api::proxy_image);
 
     let health_check = warp::path!("api" / "health")
